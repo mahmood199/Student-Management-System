b4b41da7255f0ceac0c16cf8c39d32e9c743258c
MSI KATANA <gaudentas199@gmail.com>
2023-05-01 09:06:34 +0530

    Lecture 8

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -6,6 +6,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls .static import static
 
+from .import views,hod_views,staff_views,student_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
