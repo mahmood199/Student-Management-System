b46bb1cf6e129ba62d0e1c9abbaee6dde09806b2
AdityaDas2101 <AdityaDas2101@gmail.com>
2023-05-07 22:59:51 +0530

    Lesson 27 Course and Session Select input in Show Course and Session year model data

--- student_management_system/student_management_system/hod_views.py
+++ student_management_system/student_management_system/hod_views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.template import context
 from django.contrib.auth.decorators import login_required
+from app.models import Course,Session_Year
 
 
 @login_required(login_url='/')
@@ -11,4 +12,11 @@ def HOME(request):
 
 @login_required(login_url='/')
 def ADD_STUDENT(request):
+    course = Course.objects.all()
+    session_year = Session_Year.objects.all()
+
+    context = {
+        'course':course,
+        'session_year':session_year,
+    }
     return render(request, 'Hod/add_student.html', context)
