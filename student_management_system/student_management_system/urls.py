b47caad2a187a959a89b2ddbbb017e5a0014f285
AdityaDas2101 <AdityaDas2101@gmail.com>
2023-05-08 18:16:53 +0530

    Lesson 34 Update Student (Backends)

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -14,22 +14,23 @@ class Hod_Views:
 
 
 urlpatterns = {
-                  path('admin/', admin.site.urls),
-                  path('base/', views.BASE, name='base'),
+                path('admin/', admin.site.urls),
+                path('base/', views.BASE, name='base'),
 
-                  # Login Path
-                  path('', views.LOGIN, name='login'),
-                  path('doLogin', views.doLogin, name='doLogin'),
-                  path('doLogout', views.doLogout, name='logout'),
+                # Login Path
+                path('', views.LOGIN, name='login'),
+                path('doLogin', views.doLogin, name='doLogin'),
+                path('doLogout', views.doLogout, name='logout'),
 
-                  # Profile Update
-                  path('Profile', views.PROFILE, name='profile'),
-                  path('Profile/update',views.PROFILE_UPDATE,name='profile_update'),
+                # Profile Update
+                path('Profile', views.PROFILE, name='profile'),
+                path('Profile/update',views.PROFILE_UPDATE,name='profile_update'),
 
-                  # HOD Panel URL
-                  path('Hod/Home', hod_views.HOME, name='hod_home'),
-                  path('Hod/Student/Add',hod_views.ADD_STUDENT,name='add_student'),
-                  path('Hod/Student/View',hod_views.VIEW_STUDENT,name='view_student'),
-                  path('Hod/Student/Edit/<str:id>',hod_views.EDIT_STUDENT,name='edit_student'),
+                # HOD Panel URL
+                path('Hod/Home', hod_views.HOME, name='hod_home'),
+                path('Hod/Student/Add',hod_views.ADD_STUDENT,name='add_student'),
+                path('Hod/Student/View',hod_views.VIEW_STUDENT,name='view_student'),
+                path('Hod/Student/Edit/<str:id>',hod_views.EDIT_STUDENT,name='edit_student'),
+                path('Hod/Student/Update',hod_views.UPDATE_STUDENT,name='update_student'),
 
               } + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
