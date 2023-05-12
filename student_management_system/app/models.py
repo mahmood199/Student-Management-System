c1f1fb0eb34f571a01944a711191c2ebbddcf2cf
AdityaDas2101 <AdityaDas2101@gmail.com>
2023-05-12 19:44:21 +0530

    Lesson 69 Create Staff Feedback Model

--- student_management_system/app/models.py
+++ student_management_system/app/models.py
@@ -78,11 +78,23 @@ class Staff_leave(models.Model):
     data = models.CharField(max_length=100)
     message = model.TextField()
     status = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_new_add=True)
-    updated_at = models.DateTimeField(auto_new_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.staff_id.admin.first_name + self.staff_id.admin.last_name
+
+
+
+class Staff_Feedback(models.Model):
+    staff_id = models.ForeignKey(Staff,on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.staff_id.admin.first_name + " " + self.staff_id.admin.last_name
 
 
 
