360ea6981cd34df28f7312e6efc51d37bdfef2a7
MSI KATANA <mohammed.mahmood@semaai.com>
2023-05-27 01:45:24 +0530

    Fresh new models added to db. for the exam section

---
+++ student_management_system/Notes.py
@@ -0,0 +1,47 @@
+#
+#
+# Retrieve a course
+# course = Course.objects.get(name='btech')
+#
+# Retrieve sessions for the course
+# sessions = course.session_set.all()
+#
+# Iterate over the sessions
+# for session in sessions:
+#    print(session.session_start, session.session_end)
+#
+# This code retrieves the "btech" course,
+# and then accesses all the sessions associated
+# with that course using the reverse relation session_set.
+# You can then perform any required operations on the sessions.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
